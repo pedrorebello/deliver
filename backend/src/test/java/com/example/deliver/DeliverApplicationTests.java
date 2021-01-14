02b7230be10ad2fe574adbbd847c209f34e829bf
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2021-01-13 22:47:41 -0500

    Initial Commit

---
+++ backend/src/test/java/com/example/deliver/DeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
