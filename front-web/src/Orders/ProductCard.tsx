9fbd4cc3f808c4527cbd578cd9c745ea7fced796
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2021-01-22 12:13:18 -0500

    Add dynamic product cards list

---
+++ front-web/src/Orders/ProductCard.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+import { Product } from "./types";
+
+type Props = {
+  product: Product;
+}
+
+function formatPrice(price: number) {
+  const formatter = new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD'
+  });
+
+  return formatter.format(price);
+}
+
+function ProductCard({ product }: Props) {
+  return (
+    <div className="order-card-container">
+      <h3 className="order-card-title">
+        {product.name}
+      </h3>
+      <img 
+        src={product.imageUri} 
+        className="order-card-image"
+        alt={product.name} 
+      />
+      <h3 className="order-card-price">
+        {formatPrice(product.price)}
+      </h3>
+      <div className="order-card-description"> 
+        <h3>Description</h3>
+        <p>
+          {product.description}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
